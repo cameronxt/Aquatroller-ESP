a79feb260076db4d30ddd5054a3520e32164cd21
cameronxt <cameronxt@gmail.com>
2021-09-28 23:02:59 -0500

    fixed several ATO bugs

--- ato.cpp
+++ ato.cpp
@@ -1,122 +1,157 @@
 #include "ato.h"
 
+// Set all pin constants
 AutoTopOff::AutoTopOff (byte opticalSwitchPin, byte alarmSwitchPin, byte waterValvePin)
   : _opticalSwitchPin(opticalSwitchPin), _alarmSwitchPin(alarmSwitchPin), _waterValvePin(waterValvePin), _bufferSize (DEBOUNCE_BUFFER)  // assign control pins
 {
 }
 
 void AutoTopOff::init() {  // Setup control pins
+  // set correct pinModes
   pinMode(_opticalSwitchPin, INPUT);
   pinMode(_alarmSwitchPin, INPUT);
   pinMode(_waterValvePin, OUTPUT);
+
+  digitalWrite(_waterValvePin, LOW);  // Set water valve to off on startup
 }
 
+
+// Check mechanical float sensor for an alrarm (overfull) condition
 void AutoTopOff::alarmLoop() {
-  unsigned long delay;
-  if ( isFilling() ) {
-    delay = _data.AlarmCheckDelayWhileFilling;  // timer for delay
+  ESP_LOGV("ATO", "Checking for alarms");
+  unsigned long delay;    //required delay in millis
+  if ( isFilling() ) {    // Set appropriate delay based on whether filling
+    delay = _data.AlarmCheckDelayWhileFilling;  
   } else {
     delay = _data.AlarmCheckDelay;
   }
-  if (checkForAlarm()) {                                                       // if alarm switch has been triggered
-    disableATOValve();                                                         // turn of the valve so it doesnt overflow
-  } else {                                                                     // if alarm is no longer triggered we can reset the alarm
+  if (checkForAlarm()) {   // if alarm switch has been triggered
+    disableATOValve();  // turn of the valve so it doesnt overflow
+  } else {  // otherwise we can reset the alarm
     resetAlarm();
   }
+  ESP_LOGD("ATO", "Alarm check delay is %i", delay);
   vTaskDelay(delay / portTICK_PERIOD_MS);
 }
 
 
 void AutoTopOff::valveLoop() {
+  ESP_LOGD("ATO", "Checking Level via Optical sensor");
   unsigned long delayValve = 0;
-  if ( isFilling() ) {                                    // if valve is open
-    delayValve =_data.LevelCheckDelayWhileFilling;        // timer for delay
-    if (checkLevelSensor()) {                             // if true, water level is full
-      disableATOValve();                                  //So lets turn it off
+  if ( isFilling() ) {   // if valve is open
+    delayValve =_data.LevelCheckDelayWhileFilling; // timer for delay
+    if (checkLevelSensor()) {  // if true, water level is full
+      disableATOValve();  //So lets turn it off
     }
-  } else {                                                // otherwise valve is closed
-    delayValve = _data.LevelCheckDelay;                   // timer for delay
-    enableATOValve();                                     // lets fill it up
+  } else {  // otherwise valve is closed
+    delayValve = _data.LevelCheckDelay;  // timer for delay
+    enableATOValve();   // lets fill it up
   }
-  vTaskDelay(delayValve / portTICK_PERIOD_MS);            // time to delay in ms
+  vTaskDelay(delayValve / portTICK_PERIOD_MS);  // time to delay in ms
 }
 
-
+// Returns true if there is an active alarm
 bool AutoTopOff::isAlarmActive() {
+  if (_overfillAlarm) {
+  ESP_LOGV("ATO", "Valve: ATO Filling");
+  } else { 
+    ESP_LOGV("ATO", "Valve: idle");
+  }
   return _overfillAlarm;
 }
 
-
+// Resets alarm if it was triggered
 void AutoTopOff::resetAlarm() {
-  if (isAlarmActive()) {  // Only reset if its been set
+  if (isAlarmActive()) { 
+    ESP_LOGD("ATO", "Resetting Alarm");
     _overfillAlarm = false;
   }
 }
 
-// true is full and false is low
-bool AutoTopOff::checkLevelSensor() {
-  unsigned long delayLevelSensor;
-  for (byte i; i < sizeof(_levelBufferIndex); i++) {
-    _levelBuffer[i] = analogRead(_opticalSwitchPin);
-    if (isFilling()) {
-      delayLevelSensor = getOpticalCheckFillingDelay();
-    } else {
-      delayLevelSensor = getOpticalCheckDelay();
-    }
-    vTaskDelay(delayLevelSensor / portTICK_PERIOD_MS);
-  }
 
-  int sum = 0;
-  for (int index = 0; index < (sizeof(_levelBuffer)); index++) {  // add buffer readings together
-    sum += _levelBuffer[index];
-  }
-  if ((sum / sizeof(_levelBuffer)) > _data.OptoThreshold) {  // if the average of the buffer readings above threshold
-    return true;                                             // above threshold = full
+bool AutoTopOff::isFilling() { 
+  if ( _isValveOpen ) {
+    ESP_LOGD("ATO", "Valve is open");
   } else {
-    return false;  // below threshold = needs filled
+    ESP_LOGD("ATO", "Valve is closed");
   }
-  vTaskDelay(1);
+  return _isValveOpen;
 }
 
+// Check optical level sensor and then returns
+//  true when full and false when low
+bool AutoTopOff::checkLevelSensor() {
+  unsigned long delayLevelSensor;
+  int sum = 0;
+  ESP_LOGV("ATO", "Checking optical level sensor");
+  for (byte i = 0; i < DEBOUNCE_BUFFER; i++) {  // cycle through buffer
+    _levelBuffer[i] = analogRead(_opticalSwitchPin);  // store optical switch value into buffer
+    sum += _levelBuffer[i];
+    ESP_LOGD("ATO", "Adding %imv to buffer in position %i", _levelBuffer[i], i);
+    vTaskDelay(50);
+  }
+  if (isFilling()) {    // Set required delay depending on if we are filling or not
+    delayLevelSensor = getOpticalCheckFillingDelay();
+  } else {
+    delayLevelSensor = getOpticalCheckDelay();
+  }
+  ESP_LOGD("ATO", "Total Sum: %i", sum);
+  ESP_LOGD("ATO", "Reported Value: %i", sum/sizeof(_levelBuffer));
+
+  if ((sum / DEBOUNCE_BUFFER) > _data.OptoThreshold) {  // if the average of the buffer readings above threshold
+    ESP_LOGD("ATO", "Tank is full");
+    return true;   // above threshold = full
+  } else {
+    ESP_LOGD("ATO", "Tank needs filled");
+    return false;  // below threshold = needs filled
+  }
+  // vTaskDelay(1);
+}
+
+// Check for an alarm condition returns true when
+//  all buffer poitions return an alarm
 bool AutoTopOff::checkForAlarm() {
   bool alarm = digitalRead(_alarmSwitchPin);
-  _prevLevelTime = _currentTime;                         // Check alarm float switch
-  _prevAlarmCheckTime = _currentTime;                    // Update time last checked
-  _alarmBuffer[_alarmBufferIndex] = alarm;               // Store reading into buffer
-  if (_alarmBufferIndex < (sizeof(_alarmBuffer) - 1)) {  // increase index buffer if there is room
+  _alarmBuffer[_alarmBufferIndex] = alarm;   // Store reading into buffer
+  int sum = 0;  // for storing the sum of al the buffer positions to average
+  if (_alarmBufferIndex < sizeof(_alarmBuffer) -1 ) {  // increase index buffer if there is room
     _alarmBufferIndex++;
   } else {  // otherwise reset it
     _alarmBufferIndex = 0;
   }
-  int sum = 0;
-  for (int index = 0; index < (sizeof(_alarmBuffer) - 1); index++) {  // Count all positions of alarm switch
+  for (int index = 0; index < sizeof(_alarmBuffer); index++) {  // Sum all positions of alarm switch from buffer
     if (_alarmBuffer[index]) {
       sum++;
     }
   }
   if ((sum >= sizeof(_alarmBuffer))) {  // if the buffer is full of alarms
-    _overfillAlarm = true;              // set overfill alarm
-    return true;                        // true = alarm
+    _overfillAlarm = true;  // set overfill alarm
+    return true;   // true = alarm
   }
-  return false;  // false = normal
+  _overfillAlarm = false;
+  return false;  // false = no alarm
 }
 
-
+// Turns on ATO fill valve if its off and has been longer than required delay
 void AutoTopOff::enableATOValve() {
-  if (!_isValveOpen) {                                            // If valve is not open
-    if (_currentTime - _prevValveTime > _data.WaterValveDelay) {  // valve cycle timer
-      digitalWrite(_waterValvePin, HIGH);                         // Set valve open
+  if (!_isValveOpen) {   // If valve is not open
+    if (millis() - _prevValveTime > _data.WaterValveDelay) {  // valve cycle timer
+    ESP_LOGI("ATO", "Opening Valve");
+      digitalWrite(_waterValvePin, HIGH);  // Set valve open
       _isValveOpen = true;  // set flag
-      _data.WaterValveDelay = _currentTime;  // Set last valve cycle time
+      _prevValveTime = millis();  // Set last valve cycle time
     }
   }
 }
 
 void AutoTopOff::disableATOValve() {
-  if (_isValveOpen) {                   // if valve open
-    digitalWrite(_waterValvePin, LOW);  // Set valve closed
-    _isValveOpen = false; // reset flag
-    _data.WaterValveDelay = _currentTime;  // Set last valve cycle time
+  if (_isValveOpen) {  // if valve open
+    if (millis() - _prevValveTime > _data.WaterValveDelay) {  // valve cycle timer
+    ESP_LOGI("ATO", "Closing Valve");
+      digitalWrite(_waterValvePin, LOW);  // Set valve closed
+      _isValveOpen = false; // reset flag
+      _prevValveTime = millis();  // Set last valve cycle time
+    }
   }
 }
 
--- ato.h
+++ ato.h
@@ -3,8 +3,9 @@
 #include "Arduino.h"
 
 ////////////////////////////////////////////////////////////
-///////////////////TEST THIS!!!!!!!!!!//////////////////////
+///////////////////TEST THIS ALL!!!!!!//////////////////////
 ////////////////////////////////////////////////////////////
+
 const int DEBOUNCE_BUFFER = 3;
 // Data structure for random access to stored eeprom values
 struct ATOData {
@@ -24,58 +25,64 @@ struct ATOData {
 class AutoTopOff{
 
   public:
-  AutoTopOff(byte opticalSwitchPin, byte alarmSwitchPin, byte waterValvePin);
-  void init();
-  void alarmLoop();   // Main Loop
-  void valveLoop();
-  
-  bool needsFilled();   // Check fluid level, returns true when below fill level
-  bool isAlarmActive();   // Checks for alarm condiion, if alram return true
-  void resetAlarm();
-  bool isFilling() { return _isValveOpen;};
+    AutoTopOff(byte opticalSwitchPin, byte alarmSwitchPin, byte waterValvePin);
+    void init();
+    void alarmLoop();   // Main Loop
+    void valveLoop();
+    
+    bool needsFilled();   // Check fluid level, returns true when below fill level
+    bool isAlarmActive();   // Checks for alarm condiion, if alram return true
+    void resetAlarm();
+    bool isFilling();
 
-  // Check Sensors
-  bool checkLevelSensor();
-  bool checkForAlarm();
+    // Sensor Monitoring
+    bool checkLevelSensor();
+    bool checkForAlarm();
 
-  // Turns ATO on/off respectively
-  void enableATOValve();
-  void disableATOValve();
+    // Valve control
+    void enableATOValve();
+    void disableATOValve();
 
-  // Setters
-  void setOpticalCheckDelay(unsigned long lvlCheckDelay);
-  void setOpticalCheckFillingDelay(unsigned long lvlCheckDelay);
-  void setAlarmCheckDelay(unsigned long alarmCheckDelay);
-  void setAlarmCheckFillingDelay(unsigned long alarmCheckDelay);
-  void setValveCycleDelay(unsigned long valveCycleDelay);
-  void setOptoThreshold(int threshold);
+    // Setters
+    void setOpticalCheckDelay(unsigned long lvlCheckDelay);
+    void setOpticalCheckFillingDelay(unsigned long lvlCheckDelay);
+    void setAlarmCheckDelay(unsigned long alarmCheckDelay);
+    void setAlarmCheckFillingDelay(unsigned long alarmCheckDelay);
+    void setValveCycleDelay(unsigned long valveCycleDelay);
+    void setOptoThreshold(int threshold);
 
-  // Getters
-  unsigned long getOpticalCheckDelay();
-  unsigned long getOpticalCheckFillingDelay();
-  unsigned long getAlarmCheckDelay();
-  unsigned long getAlarmCheckFillingDelay();
-  unsigned long getValveCycleDelay();
-  int getOptoThreshold();
-// Get valve position function
+    // Getters
+    unsigned long getOpticalCheckDelay();
+    unsigned long getOpticalCheckFillingDelay();
+    unsigned long getAlarmCheckDelay();
+    unsigned long getAlarmCheckFillingDelay();
+    unsigned long getValveCycleDelay();
+    int getOptoThreshold();
+  // Get valve position function
 
   private:
-  ATOData _data;
-  const byte _opticalSwitchPin;
-  const byte _alarmSwitchPin;
-  const byte _waterValvePin;
-  const byte _bufferSize;
-  
-  bool _isValveOpen = false;
-  bool _overfillAlarm = false;
-  unsigned long _prevLevelTime = 0;
-  unsigned long _prevAlarmCheckTime = 0;
-  unsigned long _prevValveTime = 0;
-  unsigned long _currentTime = 0;
-  int _levelBuffer[DEBOUNCE_BUFFER] = {0,0,0}; 
-  bool _alarmBuffer[DEBOUNCE_BUFFER] = {false,false,false};
-  byte _levelBufferIndex = 0;
-  byte _alarmBufferIndex = 0;
+    ATOData _data;    // User editable values
+
+    /////// constants declared in constructor ////////
+    const byte _opticalSwitchPin;
+    const byte _alarmSwitchPin;
+    const byte _waterValvePin;
+    const byte _bufferSize;
+    
+    //Buffers
+    bool _alarmBuffer[DEBOUNCE_BUFFER]; // alarm state buffer
+    int _levelBuffer[DEBOUNCE_BUFFER];  // level state buffer
+
+    // Indices for buffers
+    byte _levelBufferIndex = 0;   // Current index for opto level sensor buffer
+    byte _alarmBufferIndex = 0;   // current index for alarm switch buffer
+
+    // Class use variables
+    bool _isValveOpen = false;  // Valve state
+    bool _overfillAlarm = false;  // Alarm state
+    unsigned long _prevLevelTime = 0;   // Last time the level was checked with opto
+    unsigned long _prevAlarmCheckTime = 0;   // Last time we checked for an alarm
+    unsigned long _prevValveTime = 0;   // Last time the valve was cycled
   
 };
 #endif
